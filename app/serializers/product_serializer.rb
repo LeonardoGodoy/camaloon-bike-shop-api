87eeb377b7524864e5338c60a096b592c53cb856
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2021-03-13 19:57:14 -0300

    Annotate product serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,24 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :uuid             not null, primary key
+#  description :string
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :uuid
+#
+# Indexes
+#
+#  index_products_on_category_id  (category_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (category_id => categories.id)
+#
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :updated_at,
              :title, :description
