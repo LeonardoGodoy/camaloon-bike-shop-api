1f57319fb798fa95b59df8ea5c9055780f8d563b
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2021-03-13 16:48:28 -0300

    Add customer seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Customer.create(name: 'Camaloon Customer', email: 'camaloon_customer@gmail.com')
