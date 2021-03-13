1f0d1690be9623cddb2ecca7c74b2b050149fd6e
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2021-03-12 22:00:14 -0300

    Implement customer factory

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: customers
+#
+#  id         :uuid             not null, primary key
+#  email      :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 FactoryBot.define do
   factory :customer do
     sequence :name do |n|
