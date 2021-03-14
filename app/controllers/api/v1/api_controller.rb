bf9c05f57525c8db8a2d21541cc52962cfd6b389
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2021-03-14 08:52:28 -0300

    Add API error messages

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -8,12 +8,22 @@ module Api
       def authenticate
         return if current_customer.present?
 
-        head :unauthorized
+        render json: {
+          message: I18n.t('api.v1.error_messages.unauthorized')
+        }, status: :unauthorized
       end
 
       def current_customer
         # TODO: Users auth: Get token from headers...
         @current_customer ||= Customer.first
+      end
+
+      def check(object_key)
+        return if send(object_key).present?
+
+        render json: {
+          message: I18n.t('api.v1.error_messages.record_not_found')
+        }, status: :unprocessable_entity
       end
     end
   end
