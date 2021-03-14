9414c306edf90585db143dbbaa5979908d93d9f1
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2021-03-14 10:49:16 -0300

    Move check to application controller

--- app/controllers/api/v1/admin/admin_api_controller.rb
+++ app/controllers/api/v1/admin/admin_api_controller.rb
@@ -14,14 +14,6 @@ module Api
           #   message: I18n.t('api.v1.error_messages.unauthorized')
           # }, status: :unauthorized
         end
-
-        def check(object_key)
-          return if send(object_key).present?
-
-          render json: {
-            message: I18n.t('api.v1.error_messages.record_not_found')
-          }, status: :unprocessable_entity
-        end
       end
     end
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  def check(object_key)
+    return if send(object_key).present?
+
+    render json: {
+      message: I18n.t('api.v1.error_messages.record_not_found')
+    }, status: :unprocessable_entity
+  end
 end
