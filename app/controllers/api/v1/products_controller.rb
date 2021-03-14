1d28a38c409336347fa709c76567d49439492095
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2021-03-14 10:49:39 -0300

    Add customers product controller

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class ProductsController < ApiController
+      before_action :check_product, only: %i[show]
+
+      def index
+        products = Product.all
+
+        render json: products, status: :ok
+      end
+
+      def show
+        render json: product, status: :ok
+      end
+
+      private
+
+      def product
+        @product ||= Product.find_by(id: params[:id])
+      end
+
+      def check_product
+        check(:product)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
           end
         end
       end
+
+      resources :products, only: %i(index show)
     end
   end
 end
