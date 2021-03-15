c5cfde0ffa8a0004fbc1dc63ad218462b8363897
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2021-03-14 22:04:14 -0300

    Create product version property values

--- app/services/create_product_version.rb
+++ app/services/create_product_version.rb
@@ -11,8 +11,7 @@ class CreateProductVersion
   def perform
     product_version.save
 
-    # TODO: Verify necessity of property_values association
-    # product_version.values.create(...)
+    product_version.property_values.create(properties_values_attributes)
   end
 
   def product_version
@@ -26,5 +25,11 @@ class CreateProductVersion
       enabled: true,
       property_combination: property_combination
     }
+  end
+
+  def properties_values_attributes
+    property_combination.map do |property_values|
+      property_values.slice(:property_id, :value)
+    end
   end
 end
