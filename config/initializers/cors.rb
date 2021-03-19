844d6452b1b2206d6eb22440a464b52819482e73
Leonardo Godoy <leonardo.godoy@trackmob.com.br>
2021-03-19 01:20:39 -0300

    Allow cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    origins 'localhost:3000', 'http://camaloonbikeshop.s3-website-us-east-1.amazonaws.com'
 
     resource '*',
       headers: :any,
